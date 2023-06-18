e678e6500324a6f8e263ad184817773cfd82b274
Ummadi Rama Rangeswara Reddy <ramarangeswara.reddy@sen-sei.in>
2023-06-18 12:05:59 +0530

    edit componet

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -7,15 +7,21 @@ import { IStudents } from './models/students';
 })
 export class DataService {
 
-  constructor() { }
-   getStudents(): Observable<IStudents[]> {
-    return of(this.STUDENTS);
-  }
-
   STUDENTS: IStudents[] = [
     { id: 1, name: 'Rama', age: 23, address: 'kgPalle', dateOfBirth: '23/11/1998' },
-    { id: 1, name: 'Chandra', age: 37, address: 'Brahpalle', dateOfBirth: '03/07/2001' },
-    { id: 1, name: 'Mali', age: 23, address: 'Yuguduru', dateOfBirth: '13/04/1898' },
-    { id: 1, name: 'Neela', age: 23, address: 'Bugga', dateOfBirth: '28/07/1978' }
+    { id: 2, name: 'Chandra', age: 37, address: 'Brahpalle', dateOfBirth: '03/07/2001' },
+    { id: 3, name: 'Mali', age: 23, address: 'Yuguduru', dateOfBirth: '13/04/1898' },
+    { id: 4, name: 'Neela', age: 23, address: 'Bugga', dateOfBirth: '28/07/1978' }
   ]
+  constructor() { }
+  getStudents(): Observable<IStudents[]> {
+    return of(this.STUDENTS);
+  }
+   getStudent(id:number):Observable<IStudents>{
+    if(this.STUDENTS.length>=id){
+      return of(this.STUDENTS[id-1]);
+    }
+    return of(this.STUDENTS[0]);
+  }
+
 }
--- src/app/student-edit/student-edit.component.ts
+++ src/app/student-edit/student-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import { DataService } from '../data.service';
+import { IStudents } from '../models/students';
 
 @Component({
   selector: 'app-student-edit',
@@ -6,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./student-edit.component.css']
 })
 export class StudentEditComponent {
-
+  id:number|undefined;
+  student:IStudents|undefined;
+  constructor(private route:ActivatedRoute,
+    private dataService:DataService){}
+  ngOnInit(){
+    this.id = Number(this.route.snapshot.paramMap.get('id'));
+    this.dataService.getStudent(this.id).subscribe(res=>this.student = res);
+  }
 }
