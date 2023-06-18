88c9ab9176b7be7d15c779f3e934366962c4180e
Ummadi Rama Rangeswara Reddy <ramarangeswara.reddy@sen-sei.in>
2023-06-18 11:13:31 +0530

    basic route setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+import { StudentListComponent } from './student-list/student-list.component';
+import { StudentEditComponent } from './student-edit/student-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    StudentEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(
+      [
+        { path: '', redirectTo:'/students', pathMatch:'full' },
+        { path: 'students', component: StudentListComponent },
+        { path: 'students/:id', component: StudentEditComponent }
+      ])
   ],
   providers: [],
   bootstrap: [AppComponent]
